c60cc3e89d5be1495d0e1890fd209adf92f233bf
Kristina Rudyte <rkristina32@yahoo.com>
2022-05-20 11:00:50 +0300

    week 9

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-
 console.log('---------------------');
-const africa = ['Zebras', 'Liūtas',  '', 'Raganosis', '','Raganosis', 'Begemotas'];
+const africa = [
+  'Zebras',
+  'Liūtas',
+  '',
+  'Raganosis',
+  '',
+  'Raganosis',
+  'Begemotas',
+];
 const australia = ['Kengūra', 'Ančiasnapis', 'Dingo', 'Atsirado', 'Strutis'];
 
 function rand(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 console.log('----------1-----------');
 // 1. Tiesiogiai HTML faile rankiniu būdu sukurkite du tuščius <h2> tagus ir vieną mygtuką. (2 taškai)
 // a. Paspaudus mygtuką kiekvienam tagui sugeneruotkite rand() atskirą skaičių nuo 1 iki 6 ir jį įrašykite į tago vidų naudojant innerText() metodą. (4 taškai)
+let button = document.getElementById('button');
+let content = document.getElementById('content');
+console.log(rand(1, 6));
 // b. Paspaudus mygtuką skaičiai turi būti pergeneruojami iš naujo. Jeigu sugeneruoti skaičiai yra vienodi, juos nudažyti raudonai. (4 taškai)
 console.log('----------2-----------');
 // 2. Tiesiogiai HTML faile rankiniu būdu sukurkite <h3> tagą ir vieną mygtuką. Susikurkite tuščią masyvą, skaičiams saugoti. (2 taškai)
+let masyvas = [];
+
 // a. Paspaudus mygtuką, sugeneruokite rand() skaičių nuo 1 iki 10. Sugeneruotą skaičių pridėkite į masyvą, o tą masyvą atspausdinkite konsolėle. (4 taškai)
+console.log(rand(1, 10));
+
 // b. <h3> tage skaičiuokite ir su innerText() metodu rašykite visų sugeneruotų skaičių sumą. (4 taškai)
 console.log('----------3-----------');
 // 3. Tiesiogiai HTML faile rankiniu būdu sukurkite <ul> tagą. (2 taškai)
 // a. JS pagalba perskaitykite masyvą africa. (4 taškai)
+
 // b. Naudodami createElement() ir kitus reikalingus metodus sukurkite kiekvienam masyvo elementui po <li> tagą su elemento stringu viduje ir juos įrašykite į <ul> tago vidų. (5 taškai)
 // c. Elementus su tuščiais stringais praleiskite ir jiems <li> elementų nekurkite. (4 taškai)
 console.log('----------4-----------');
@@ -32,4 +47,3 @@ console.log('---------5------------');
 // b. Tą stringą naudodami innerHTML() metodą įdėkite į <ul> tago vidų. (5 taškai)
 // c. Generavimo metu “Dingo” background’ą nuspalvinkite mėlynai. (5 taškai)
 console.log('---------------------');
-
